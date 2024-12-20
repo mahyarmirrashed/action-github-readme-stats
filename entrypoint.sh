c869d29c26d3170de955ff56a9d32d383043a7b6
Mahyar Mirrashed <mah.mirr@gmail.com>
2024-12-19 23:51:07 -0600

    fix: properly parse cmdline args

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,7 @@
 # Initialize an empty array to hold the arguments
 args=()
 
-# Use a while loop to split INPUT_INCLUDES by spaces and handle each token
-while IFS= read -r -d ' ' token; do
-  args+=("$token")
-done <<<"$INPUT_INCLUDES "
+IFS=' ' read -r -a args <<<"$1"
 
 # Pass the arguments array to your main script
 exec app "${args[@]}"
